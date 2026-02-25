270787b4cafcbebe8de785c76ae8a5c027551ef1
Jamilson <59518273+JamilsonMello@users.noreply.github.com>
2026-02-25 00:05:05 -0300

    remove all

--- gateway/repository/user_repository.ts
+++
@@ -1,8 +0,0 @@
-export interface UserRepository {
-  getUserByEmail(email: string): Promise<any>;
-  storePasswordRecoveryToken(userId: string, token: string): Promise<void>;
-  getUserIdByPasswordRecoveryToken(token: string): Promise<string | null>;
-  updatePassword(userId: string, hashedPassword: string): Promise<void>;
-  invalidatePasswordRecoveryToken(userId: string): Promise<void>;
-  listUsers(filters: { name?: string, email?: string }, pagination: { skip: number, limit: number }, sort: { [key: string]: 'asc' | 'desc' }): Promise<any[]>;
-}
